43c1a98d4ab9ae9b28cba3bfaccd7ac0441c3c3d
JamaTec Development Team <prostniv@jamatec.nl>
2025-09-08 07:32:16 +0200

    Clean up: Remove .next build folder from version control
    - Removed .next folder from git tracking
    - This folder contains build artifacts and should not be committed
    - Added proper .gitignore to prevent future build artifacts from being tracked

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VzpkKdd7W4Krp2a/4G3zObgY3QSE8JHpD4tlFfPPsyA=\"\n}"
--- .next/static/chunks/a08a584fbef23b39.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/cf776a65d4a8e936.js",
-  "static/chunks/turbopack-2700cfe8e4d089e2.js"
-])
